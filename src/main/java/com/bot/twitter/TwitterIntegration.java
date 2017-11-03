6fca7cb1181251f7f9bd1eb752d8488cfd01e27c
Angela Lukic <angelalukic@live.co.uk>
2017-11-03 09:45:15 +0000

    Clean up Twitter method & have embedded output

--- src/main/java/com/bot/twitter/TwitterIntegration.java
+++ src/main/java/com/bot/twitter/TwitterIntegration.java
@@ -1,6 +1,12 @@
 package com.bot.twitter;
 
+import java.awt.Color;
+
 import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.entities.message.embed.EmbedBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import twitter4j.FilterQuery;
 import twitter4j.StallWarning;
 import twitter4j.Status;
@@ -10,51 +16,76 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+@Getter
+@Setter
+@Slf4j
 public class TwitterIntegration {
 	
 	Message message;
+	
+	private static final long TWITTER_ID = /**/;
+	private static final String BOT_FEED_CHANNEL = /**/;
+	private static final String TWITTER_CONSUMER_KEY = /**/;
+	private static final String TWITTER_CONSUMER_SECRET = /**/;
+	private static final String TWITTER_ACCESS_TOKEN = /**/;
+	private static final String ACCESS_TOKEN_SECRET = /**/;
 	
 	public TwitterIntegration(Message message) {
 		this.message = message;
 	}
 	
-	public void test() {
+	public void retrieveStream() {
 		
-		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder.setOAuthConsumerKey("SK9sjuc9HINWNiEhnwVnzEStY")
-                .setOAuthConsumerSecret("KY957k34vy6z6KvEtxvyVmvkcKl2xQTwSz1ZnIt2yNCGPdfHwE")
-                .setOAuthAccessToken("780352244080336896-MmKjV98lPekthHQVtpUoFgr6gOJzUm1")
-                .setOAuthAccessTokenSecret("zbxhxkKXHzR56l2WfYmzfSHNuaSH2zuPhV58DHvEwNHOQ");
+		TwitterStream twitterStream = buildTwitterStream();
         
-        TwitterStream twitterStream = new TwitterStreamFactory(configurationBuilder.build()).getInstance();
-        
-        StatusListener listener = new StatusListener(){
-            public void onStatus(Status status) {
-            	message.getChannelReceiver().getServer().getChannelById("375672676859248660").sendMessage(status.getText());
-            }
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-			public void onScrubGeo(long arg0, long arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-			public void onStallWarning(StallWarning arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-        };
-        
+        StatusListener listener = createListener();
         twitterStream.addListener(listener);
         
-        FilterQuery filterQuery = new FilterQuery();
-        
-        filterQuery.follow(new long[]{780352244080336896L});
-        
-        twitterStream.filter(filterQuery);
-		
+        addFilterQuery(twitterStream);		
 	}
 
+	private TwitterStream buildTwitterStream() {
+		
+		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey(TWITTER_CONSUMER_KEY)
+                .setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET)
+                .setOAuthAccessToken(TWITTER_ACCESS_TOKEN)
+                .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+        
+		return new TwitterStreamFactory(configurationBuilder.build()).getInstance();
+	}
+
+	private StatusListener createListener() {
+		return new StatusListener(){
+            public void onStatus(Status status) {
+            	message.getChannelReceiver().getServer().getChannelById(BOT_FEED_CHANNEL)
+            		.sendMessage("", postStatus(status));
+            }
+            public void onException(Exception ex) {
+            	log.error(ex.getMessage());
+            }
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) { /*Do nothing*/ }
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) { /*Do nothing*/ }
+			public void onScrubGeo(long arg0, long arg1) { /*Do nothing*/ }
+			public void onStallWarning(StallWarning arg0) { /*Do nothing*/ }
+        };
+	}
+	
+	private EmbedBuilder postStatus(Status status) {
+		
+		EmbedBuilder output = new EmbedBuilder();
+		
+		output.setTitle("Twitter: @" + status.getUser().getScreenName());
+		output.setDescription(status.getText());
+		output.setFooter(status.getCreatedAt().toString(), status.getUser().getProfileImageURL());
+		output.setColor(new Color(0,132,180));
+		
+		return output;
+	}
+	
+	private void addFilterQuery(TwitterStream twitterStream) {
+		FilterQuery filterQuery = new FilterQuery();
+        filterQuery.follow(TWITTER_ID);
+        twitterStream.filter(filterQuery);
+	}
 }
