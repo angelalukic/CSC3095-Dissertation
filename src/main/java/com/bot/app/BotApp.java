16fd7694211273eb2ac2ed1f850ab90fb2473bf9
Angela Lukic <angelalukic@live.co.uk>
2017-11-03 09:50:32 +0000

    Fix main method
    Main method no longer creates a new listener every time a new message is created

--- src/main/java/com/bot/app/BotApp.java
+++ src/main/java/com/bot/app/BotApp.java
@@ -1,6 +1,7 @@
 package com.bot.app;
 
 import com.bot.filter.WordFilter;
+import com.bot.twitter.TwitterIntegration;
 
 import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.Javacord;
@@ -11,17 +12,20 @@ public class BotApp {
 
 	public static void main(String[] args) {
 
-		DiscordAPI api = Javacord.getApi(/* KEY HERE */, true);
+		DiscordAPI api = Javacord.getApi(/**/, true);
 		api.connectBlocking();
+		
+		final TwitterIntegration twitter = new TwitterIntegration(null);
+		twitter.retrieveStream();
 
 		api.registerListener(new MessageCreateListener() {
 			public void onMessageCreate(DiscordAPI api, Message message) {
+				
+				twitter.setMessage(message);
 
-				if (message.getContent().length() > 3 
-						&& message.getContent().substring(0,3).contains("rf!")) {
-					message.reply("What is your command?");
-				}
-				else {
+				if (message.getContent().contains("rf!music")) {
+					message.reply("Soon...");
+				} else {
 					WordFilter filter = new WordFilter(message);
 					filter.checkMessage();
 				}
