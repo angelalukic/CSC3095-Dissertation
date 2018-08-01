aca84b3809659f06c94afa8e4b5299f9cda5196e
Angela Lukic <angelalukic@live.co.uk>
2018-08-01 19:20:14 +0100

    Bot loads configuration file for server, or creates new one in new server

--- src/main/java/com/bot/App.java
+++ src/main/java/com/bot/App.java
@@ -1,20 +1,44 @@
 package com.bot;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.Message;
+
+
+import com.bot.discord.ServerConfigurationLoader;
 import com.bot.yaml.YamlReader;
 
+import lombok.extern.java.Log;
+
+@Log
 public class App {
 	
     public static void main( String[] args ) {
     	
-        try {
-			System.out.println( retrieveDiscordToken() );
+		try {
+			String token = retrieveDiscordToken();
+	    	DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+	    	
+	        api.addMessageCreateListener(event -> {
+	        	
+	        	Message message = event.getMessage();
+	        	ServerConfigurationLoader configLoader = new ServerConfigurationLoader(message);
+	        	
+	        	try {       		
+					Map<String, Object> config = configLoader.retrieveConfiguration();					
+				} catch (IOException e) {
+					log.info("An error was caught when trying to retrieve configuration file:" + e.getMessage());
+				}
+	        });
+	        
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+			log.info("An error was caught when trying to retrieve properties from properties.yml" + e.getMessage());
+		} 
     }
     
     @SuppressWarnings("unchecked")
---
+++ src/main/java/com/bot/discord/ServerConfigurationLoader.java
@@ -0,0 +1,61 @@
+package com.bot.discord;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+import org.javacord.api.entity.message.Message;
+
+import com.bot.yaml.YamlReader;
+
+public class ServerConfigurationLoader {
+	
+	private Message message;
+	
+	public ServerConfigurationLoader(Message message) {
+		this.message = message;
+	}
+	
+	public Map<String, Object> retrieveConfiguration() throws IOException {
+		
+		String serverId = message.getServer().get().getIdAsString();
+		
+		if(!configExists(serverId)) {
+			createConfigFile(serverId);
+			
+			String introduction = introductoryMessage();
+			message.getChannel().sendMessage(introduction);
+		}
+		
+    	YamlReader reader = new YamlReader("servers\\" + serverId + ".yml");
+    	Map<String, Object> values = reader.retrieveYaml();
+    	
+    	return values;
+	}
+	
+	private boolean configExists(String serverId) {
+		
+		try {
+			@SuppressWarnings({ "unused", "resource" })
+			FileInputStream stream = new FileInputStream("servers\\" + serverId + ".yml");
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	private void createConfigFile(String serverId) throws IOException {
+		
+		File src = new File("servers\\template.yml");
+		File dest = new File("servers\\" + serverId + ".yml");
+		
+		Files.copy(src.toPath(), dest.toPath());
+	}
+	
+	private String introductoryMessage() {
+		return "New server detected! Introductory message with instructions as to how to set up bot will be put here ... eventually.";
+	}
+}
